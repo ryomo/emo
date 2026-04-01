1e59e84a79fee7a589d37955302cb78d6f1e2850
Ryo Moriwaki <ryomo@duck.com>
2026-04-01 15:26:16 +0900

    chore: add some directories to ESLint ignore patterns

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,11 @@ import withNuxt from './.nuxt/eslint.config.mjs'
 export default withNuxt(
   {
     ignores: [
-      'src-tauri/target/**',
+      '.nuxt/',
+      '.output/',
+      'dist/',
+      'node_modules/',
+      'src-tauri/target/',
     ],
   },
 )
