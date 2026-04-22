63b1713ed0e3fe17dcf880146bffe8839467679c
Ryo Moriwaki <ryomo@duck.com>
2026-04-22 10:22:55 +0900

    chore: update Android Gradle plugin version

--- src-tauri/gen/android/build.gradle.kts
+++ src-tauri/gen/android/build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
         maven { url = uri("https://jitpack.io") }
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.11.0")
+        classpath("com.android.tools.build:gradle:8.13.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.25")
     }
 }
