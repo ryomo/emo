82cb9bdddc3c3c13a8ecc317bb0964d142688bbd
Ryo Moriwaki <ryomo@duck.com>
2026-04-22 10:51:55 +0900

    feat: implement status bar hiding in MainActivity

--- src-tauri/gen/android/app/src/main/java/com/github/ryomo/emo/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/com/github/ryomo/emo/MainActivity.kt
@@ -2,10 +2,21 @@ package com.github.ryomo.emo
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 class MainActivity : TauriActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     enableEdgeToEdge()
     super.onCreate(savedInstanceState)
+    hideStatusBar()
+  }
+
+  private fun hideStatusBar() {
+    WindowCompat.getInsetsController(window, window.decorView).apply {
+      hide(WindowInsetsCompat.Type.statusBars())
+      systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+    }
   }
 }
